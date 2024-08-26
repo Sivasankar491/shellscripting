9428e6010432400c516cd666461ad009c99fb991
Sankar491 <sivasankarrcks@gmail.com>
2024-08-26 15:27:40 +0530

    added conversation script with variables

---
+++ 02-Variables1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2, how are you?"
+echo "$PERSON2: Hey, $PERSON1, I'm fine, whats up?"
+echo "$PERSON1: I joined a DevOps course, are you intersted?"
+echo "$PERSON2: Ofcouse! Which institute"
+echo "$PERSON1: joindevops, it's a best institute, teaching course in native language"
