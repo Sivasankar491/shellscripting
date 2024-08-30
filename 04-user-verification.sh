51de344a7273796dd2c0ad66d0b49803441c0da1
Sankar491 <sivasankarrcks@gmail.com>
2024-08-30 21:55:58 +0530

    added condition to check the user

--- 04-user-verification.sh
+++ 04-user-verification.sh
@@ -7,12 +7,13 @@ N="\e[0m"
 if [ $(whoami) != "ec2-user" ]; then
   echo -e "$R User is not ec2-user $N"
   echo "Please run the script with ec2-user"
+  exit 1
 fi
 
 if [ $# -ne 2 ]; then
   echo -e "$R Pass 2 arguements $N"
   echo -e "$R USAGE: $N $0 ARG1 ARG2"
-  exit 1
+  exit 2
 else
   echo -e "$G # of arguements satisfied$N"
 fi
