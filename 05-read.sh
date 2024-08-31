91cd053dd2aa63c2daf8d9b0b58e99050426ba87
Sankar491 <sivasankarrcks@gmail.com>
2024-08-31 11:03:56 +0530

    external value using read command

--- 05-read.sh
+++ 05-read.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #echo "enter the name"
-read -p "Enter the username:" USERNAME
-
+read -p "Enter the username:" USERNAME #REPLY is the default variable name if we don't give variable here.
+read -s "Enter the password:" PASSWORD
 echo "Entered name is: ${USERNAME}"
+echo "Entered apssword is: ${PASSWORD}"
